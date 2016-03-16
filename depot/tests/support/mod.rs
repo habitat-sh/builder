06535087d5ceba99770388c2b528346b0495c34c
Jamie Winsor <jamie@vialstudios.com>
2016-03-15 18:30:15 -0700

    split functionality into five crates from one * bldr - the initial crate mostly unchanged (for now) * core - shared functionality between any bldr crate * depot - contains the depot server * depot-core - shared functionality between any depot components * depot-client - an http client to the depot server

--- depot/tests/support/mod.rs
+++ depot/tests/support/mod.rs
@@ -4,8 +4,7 @@
 // this file ("Licensee") apply to Licensee's use of the Software until such time that the Software
 // is made available under an open source license such as the Apache 2.0 License.
 
-pub const PACKAGE_HOME: &'static str = "/opt/bldr/pkgs";
-pub const SERVICE_HOME: &'static str = "/opt/bldr/srvc";
-pub const PACKAGE_CACHE: &'static str = "/opt/bldr/cache/pkgs";
-pub const GPG_CACHE: &'static str = "/opt/bldr/cache/gpg";
-pub const KEY_CACHE: &'static str = "/opt/bldr/cache/keys";
+pub mod setup;
+pub mod path;
+pub mod command;
+pub mod docker;
