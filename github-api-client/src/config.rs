3723052f1b8345373e4e62da64a9cae030a1af15
Jamie Winsor <jamie@vialstudios.com>
2017-09-29 16:39:34 -0700

    Refactor net::oauth::github module into github-api-client crate
    We are and will continue to leverage more of the GitHub API aside
    from basic authentication. Refactoring this module out into a
    separate crate will give us a place to do that and also reduce
    the amout of dependencies required by crates requiring the net
    crate
    
    Signed-off-by: Jamie Winsor <jamie@vialstudios.com>

---
+++ github-api-client/src/config.rs
@@ -0,0 +1,58 @@
+// Copyright (c) 2016 Chef Software Inc. and/or applicable contributors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+/// URL to GitHub API endpoint
+pub const DEFAULT_GITHUB_URL: &'static str = "https://api.github.com";
+/// URL to GitHub Web endpoint
+pub const DEFAULT_GITHUB_WEB_URL: &'static str = "https://github.com";
+/// Default Client ID for providing a default value in development environments only. This is
+/// associated to the habitat-sh GitHub account and is configured to re-direct and point to a local
+/// builder-api.
+///
+/// See https://github.com/settings/connections/applications/0c2f738a7d0bd300de10
+pub const DEV_GITHUB_CLIENT_ID: &'static str = "0c2f738a7d0bd300de10";
+/// Default Client Secret for development purposes only. See the `DEV_GITHUB_CLIENT_ID` for
+/// additional comments.
+pub const DEV_GITHUB_CLIENT_SECRET: &'static str = "438223113eeb6e7edf2d2f91a232b72de72b9bdf";
+
+pub trait GitHubOAuth {
+    fn github_url(&self) -> &str;
+    fn github_web_url(&self) -> &str;
+    fn github_client_id(&self) -> &str;
+    fn github_client_secret(&self) -> &str;
+}
+
+#[derive(Clone, Debug, Deserialize, Serialize)]
+#[serde(default)]
+pub struct GitHubCfg {
+    /// URL to GitHub API
+    pub url: String,
+    /// URL to GitHub Web
+    pub web_url: String,
+    /// Client identifier used for GitHub API requests
+    pub client_id: String,
+    /// Client secret used for GitHub API requests
+    pub client_secret: String,
+}
+
+impl Default for GitHubCfg {
+    fn default() -> Self {
+        GitHubCfg {
+            url: DEFAULT_GITHUB_URL.to_string(),
+            web_url: DEFAULT_GITHUB_WEB_URL.to_string(),
+            client_id: DEV_GITHUB_CLIENT_ID.to_string(),
+            client_secret: DEV_GITHUB_CLIENT_SECRET.to_string(),
+        }
+    }
+}
