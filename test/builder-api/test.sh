07701477047a17c841dba1f8d80e3071a3d4049b
Josh Black <raskchanky@gmail.com>
2017-11-22 02:05:57 +0000

    Integration tests for the Builder HTTP API
    Resolves https://github.com/habitat-sh/builder/issues/77
    
    We had a suite of integration tests for the Builder API but it had some
    problems. It destroyed the existing PG installation, it was slow to
    start up and tear down, and it didn't cover many endpoints.
    
    This aims to rectify these problems as much as possible. It uses the
    builder dev studio environment and cleans up after itself when it exits.
    It covers as many endpoints as possible. It provides a way to run all
    of this stuff in an automated way and works both locally and on travis.
    
    It's also worth noting that we converted the tests from TypeScript to
    JavaScript since we weren't using any of the features of TypeScript and
    it was just another layer of indirection to figure out when tests
    failed.
    
    Signed-off-by: Josh Black <raskchanky@gmail.com>
    Signed-off-by: Jon Bauman <5906042+baumanj@users.noreply.github.com>

--- test/builder-api/test.sh
+++ test/builder-api/test.sh
@@ -1,34 +1,147 @@
 #!/bin/bash
 
-if [ "$I_MEAN_IT" != "1" ]; then 
-  echo "**** THESE TESTS WILL DESTROY YOUR DATABASES ****"
-  echo "**** AND I MEAN THE REALLY REAL ONES ****"
-  echo
-  echo "Set I_MEAN_IT=1 to continue"
-  exit 500
+# This script exists to run our integration tests.
+
+set -euo pipefail
+
+# make mocha happy by running from the directory it expects
+cd "$(dirname "${BASH_SOURCE[0]}")"
+
+clean_test_artifacts() {
+  if ! command -v op >/dev/null 2>&1; then
+    hab pkg install -b habitat/builder-op
+  fi
+
+  local sql origins
+  origins=( neurosis xmen )
+
+  # clean origins
+  local origins origin_id_tables origin_tables
+  origin_id_tables=( origin_members origin_channels origin_invitations origin_packages origin_projects origin_public_keys origin_secret_keys )
+  origin_tables=( origin_integrations origin_project_integrations )
+  sql=
+
+  for origin in "${origins[@]}"; do
+    sql+="SET SEARCH_PATH TO shard_$(op shard "$origin");"
+    sql+="DELETE FROM origin_channel_packages WHERE channel_id IN (SELECT id FROM origin_channels WHERE origin_id=(SELECT id FROM origins WHERE name='$origin'));"
+
+    for table in "${origin_id_tables[@]}"; do
+      sql+="DELETE FROM $table WHERE origin_id=(SELECT id FROM origins WHERE name='$origin');"
+    done
+
+    for table in "${origin_tables[@]}"; do
+      sql+="DELETE FROM $table WHERE origin='$origin';"
+    done
+
+    sql+="DELETE FROM origins WHERE name='$origin';"
+  done
+
+  psql builder_originsrv -q -c "$sql"
+
+  # clean users
+  local users account_tables
+  users=( bobo mystique )
+  account_tables=( account_invitations account_origins )
+  sql=
+
+  for user in "${users[@]}"; do
+    sql+="SET SEARCH_PATH TO shard_$(op shard "$user");"
+
+    for table in "${account_tables[@]}"; do
+      sql+="DELETE FROM $table WHERE account_id=(SELECT id FROM accounts WHERE name='$user');"
+    done
+
+    sql+="DELETE FROM accounts WHERE name='$user';"
+  done
+
+  psql builder_sessionsrv -q -c "$sql"
+
+  # clean jobs
+  sql=
+
+  for origin in "${origins[@]}"; do
+    sql+="SET SEARCH_PATH TO shard_0;"
+    sql+="DELETE FROM busy_workers WHERE job_id IN (SELECT id FROM jobs WHERE project_name LIKE '$origin%');"
+    sql+="DELETE FROM graph_packages WHERE ident LIKE '$origin%';"
+    sql+="DELETE FROM group_projects WHERE project_name LIKE '$origin%';"
+    sql+="DELETE FROM groups WHERE project_name LIKE '$origin%';"
+    sql+="DELETE FROM jobs WHERE project_name LIKE '$origin%';"
+  done
+
+  psql builder_jobsrv -q -c "$sql"
+}
+
+if [ -n "${TRAVIS:-}" ]; then
+  pushd "$(git rev-parse --show-toplevel)"
+  cp /tmp/builder-github-app.pem .secrets/
+  cp .secrets/habitat-env{.sample,}
+  support/linux/provision.sh
+  set +u; eval "$(direnv hook bash)"; set -u
+  direnv allow
+
+  # Do what `hab setup` would do
+  hab origin key generate "$(id -nu)"
+  mkdir -p "$HOME/.hab/etc"
+  cat <<EOT > "$HOME/.hab/etc/cli.toml"
+origin = "$(id -nu)"
+EOT
+  mkdir -p "$HOME/.hab/cache/analytics"
+  touch "$HOME/.hab/cache/analytics/OPTED_OUT"
+  # end hab setup
+
+  cat <<EOT >> .studiorc
+set -x
+set -euo pipefail
+
+HAB_FUNC_TEST=arg-to-sup-run sup-run
+
+until hab sup status; do echo "waiting for hab sup to start"; sleep 1; done
+
+if ! hab sup status; then
+  echo "SUPERVISOR FAILED TO START"
+  exit 2
 fi
 
-sudo rm -rf /hab/svc/postgresql
-pushd ../../
-make build-srv || exit $?
-env HAB_FUNC_TEST=1 ./support/linux/bin/forego start -f support/Procfile -e support/bldr.env 2>&1 > ./test/builder-api/services.log &
-forego_pid=$!
-popd
+start-builder
 
-echo "**** Spinning up the services ****"
-count=0;
-while [ $count -ne 5 ]; do
-  count=0;
-  for svc in builder-originsrv builder-sessionsrv builder-router builder-api builder-jobsrv; do
-    if grep -q "$svc is ready to go" ./services.log; then
-      ((count++))
-    fi
-  done
-  sleep 1
-done
-echo "**** Services ready ****"
+echo "BUILDING BUILDER"
+build-builder > /dev/null
+echo "BUILDER BUILT build-builder returned \$?"
+
+hab sup status
+
+hab pkg install -b core/node
+cd /src/test/builder-api
+npm install mocha
+hab pkg binlink core/coreutils -d /usr/bin env
+
+hab sup status
 npm run mocha
-mocha_exit_code=$?
-echo "**** Stopping services ****"
-kill -INT $forego_pid
-exit $mocha_exit_code
+exit \$?
+EOT
+  HAB_STUDIO_SUP=false hab studio enter
+else
+  clean_test_artifacts # start with a clean slate
+
+  if ! command -v npm >/dev/null 2>&1; then
+    hab pkg install -b core/node
+  fi
+
+  if ! [ -f /usr/bin/env ]; then
+    hab pkg binlink core/coreutils -d /usr/bin env
+  fi
+
+  if ! [ -d node_modules/mocha ]; then
+    npm install mocha
+  fi
+
+  if npm run mocha; then
+    echo "All tests passed, performing DB cleanup"
+    clean_test_artifacts
+  else
+    mocha_exit_code=$?
+    echo "Tests failed; skipping cleanup to facilitate investigation"
+  fi
+
+  exit ${mocha_exit_code:-0}
+fi
