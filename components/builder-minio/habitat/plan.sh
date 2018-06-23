302e6b0092ede31e72f29c5f46f0f2baebb5843b
Josh Black <raskchanky@gmail.com>
2018-06-23 00:19:27 +0000

    Pin to old versions temporarily
    Signed-off-by: Josh Black <raskchanky@gmail.com>

--- components/builder-minio/habitat/plan.sh
+++ components/builder-minio/habitat/plan.sh
@@ -3,7 +3,11 @@ pkg_version="0.1.0"
 pkg_origin=habitat
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 pkg_license=('Apache-2.0')
-pkg_deps=(core/minio core/cacerts core/openssl)
+pkg_deps=(
+  core/minio/2018-05-11T00-29-24Z/20180515134704
+  core/cacerts/2017.09.20/20171014212239
+  core/openssl/1.0.2l/20180419014054
+)
 
 do_unpack() {
     return 0
--- components/builder-worker/habitat/plan.sh
+++ components/builder-worker/habitat/plan.sh
@@ -4,11 +4,32 @@ pkg_origin=habitat
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 pkg_license=('Apache-2.0')
 pkg_bin_dirs=(bin)
-pkg_deps=(habitat/airlock core/glibc core/openssl core/gcc-libs core/zeromq core/libsodium
-  core/libarchive core/zlib core/hab-studio core/hab-pkg-export-docker
-  core/docker core/curl)
-pkg_build_deps=(core/make core/cmake core/protobuf-cpp core/protobuf-rust core/coreutils core/cacerts
-  core/rust core/gcc core/git core/pkg-config)
+pkg_deps=(
+  habitat/airlock/2374/20180530003916
+  core/hab-studio
+  core/hab-pkg-export-docker
+  core/docker/18.03.0/20180403182455
+  core/curl/7.54.1/20171014214153
+  core/glibc/2.22/20170513201042
+  core/openssl/1.0.2l/20171014213633
+  core/gcc-libs/5.2.0/20170513212920
+  core/zeromq/4.2.5/20180407102804
+  core/libsodium/1.0.13/20170905223149
+  core/libarchive/3.3.2/20171018164107
+  core/zlib/1.2.8/20170513201911
+)
+pkg_build_deps=(
+  core/make/4.2.1/20170513214620
+  core/cmake/3.10.2/20180418232649
+  core/protobuf-cpp/3.5.0/20180418230816
+  core/protobuf-rust/1.4.4/20180418221745
+  core/coreutils/8.25/20170513213226
+  core/cacerts/2017.09.20/20171014212239
+  core/rust/1.26.2/20180606182054
+  core/gcc/5.2.0/20170513202244
+  core/git/2.14.2/20180416203520
+  core/pkg-config/0.29/20170513212944
+)
 pkg_binds=(
   [jobsrv]="worker_port worker_heartbeat log_port"
   [depot]="url"
