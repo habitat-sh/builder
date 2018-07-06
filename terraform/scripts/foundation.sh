de6e13449d550346bc96de361ead6aea1e4fc0d7
Christopher Maier <cmaier@chef.io>
2018-07-06 15:12:09 -0400

    Mount drive to /mnt, NOT /hab
    This made for unhappy VMs after a restart.
    
    Signed-off-by: Christopher Maier <cmaier@chef.io>

--- terraform/scripts/foundation.sh
+++ terraform/scripts/foundation.sh
@@ -6,7 +6,7 @@
 set -eux
 
 sudo mount /dev/xvdf /mnt
-echo '/dev/xvdf /hab     ext4   defaults 0 0' | sudo tee -a /etc/fstab
+echo '/dev/xvdf /mnt     ext4   defaults 0 0' | sudo tee -a /etc/fstab
 sudo mkdir -p /mnt/hab
 sudo ln -s /mnt/hab /hab
 
