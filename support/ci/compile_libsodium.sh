ca95e1e5a0e5b53944c924159e2f7964d69dbf63
Jon Bauman <5906042+baumanj@users.noreply.github.com>
2018-07-18 09:56:22 -0400

    Fix shellcheck lints for support scripts
    Signed-off-by: Jon Bauman <5906042+baumanj@users.noreply.github.com>

--- support/ci/compile_libsodium.sh
+++ support/ci/compile_libsodium.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-# shellcheck disable=SC2046,SC2086
 set -eu
 
 version=1.0.13
@@ -14,12 +13,12 @@ if [ -d "$prefix" ]; then
   exit 0
 fi
 
-source $(dirname $0)/rust_env.sh
+source "$(dirname "$0")"/rust_env.sh
 
 echo "--> Compiling $nv"
 trap 'rm -rf /tmp/${nv}*; exit $?' INT TERM EXIT
-(cd /tmp && wget $source && tar xf /tmp/$(basename $source))
+(cd /tmp && wget $source && tar xf /tmp/"$(basename $source)")
 cd /tmp/$nv
-./configure --prefix=$prefix
+./configure --prefix="$prefix"
 make
 make install
