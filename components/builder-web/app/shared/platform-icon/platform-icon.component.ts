8e8459d008562b342d8dd61220a20f514838f6b5
Christian Nunciato <chris@nunciato.org>
2018-09-20 14:15:43 -0700

    Handle Kernel v2 in Builder UI
    This adds an icon for packages targeting Linux kernel version 2.
    
    Signed-off-by: Christian Nunciato <chris@nunciato.org>

--- components/builder-web/app/shared/platform-icon/platform-icon.component.ts
+++ components/builder-web/app/shared/platform-icon/platform-icon.component.ts
@@ -3,7 +3,7 @@ import { targetToPlatform } from '../../util';
 
 @Component({
   selector: 'hab-platform-icon',
-  template: `<hab-icon [symbol]="os" class="icon-os" [title]="os | titlecase"></hab-icon>`
+  template: `<hab-icon [symbol]="os" class="icon-os" [title]="title"></hab-icon>`
 })
 export class PlatformIconComponent {
 
@@ -11,5 +11,13 @@ export class PlatformIconComponent {
 
   get os() {
     return targetToPlatform(this.platform);
+  }
+
+  get title() {
+    return {
+      linux: 'Linux',
+      kernel2: 'Linux (Kernel Version 2)',
+      windows: 'Windows'
+    }[this.os] || '';
   }
 }
