0e5458e8f9935589bab7ad40be560856ce2bb147
Salim Alam <salam@chef.io>
2018-09-24 17:51:24 -0700

    More small cleanup
    Signed-off-by: Salim Alam <salam@chef.io>

--- components/builder-minio/habitat/plan.sh
+++ components/builder-minio/habitat/plan.sh
@@ -3,6 +3,7 @@ pkg_origin=habitat
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 pkg_license=('Apache-2.0')
 pkg_deps=(core/minio core/cacerts core/openssl core/aws-cli)
+pkg_build_deps=(core/git)
 
 pkg_version() {
   # TED: After migrating the builder repo we needed to add to
