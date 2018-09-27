7402270a863a206abbb9c79a76c17cbbefe4e568
Elliott Davis <elliott@excellent.io>
2018-09-27 10:20:46 -0500

    Update Terraform config to allow api to talk to datastore
    Signed-off-by: Elliott Davis <elliott@excellent.io>

--- terraform/instances.tf
+++ terraform/instances.tf
@@ -12,6 +12,7 @@ resource "aws_instance" "api" {
     "${var.aws_admin_sg}",
     "${var.hab_sup_sg}",
     "${var.events_sg}",
+    "${aws_security_group.datastore_client.id}",
     "${aws_security_group.gateway.id}",
   ]
 
@@ -94,7 +95,7 @@ resource "aws_instance" "api" {
       "sudo systemctl start hab-sup",
       "sudo systemctl enable hab-sup",
       "sudo hab svc load habitat/builder-memcached --group ${var.env} --strategy at-once --url ${var.bldr_url} --channel ${var.release_channel}",
-      "sudo hab svc load habitat/builder-api --group ${var.env} --bind memcached:builder-memcached.${var.env} --bind router:builder-router.${var.env} --strategy at-once --url ${var.bldr_url} --channel ${var.release_channel}",
+      "sudo hab svc load habitat/builder-api --group ${var.env} --bind memcached:builder-memcached.${var.env} --bind datastore:builder-datastore.${var.env} --bind router:builder-router.${var.env} --strategy at-once --url ${var.bldr_url} --channel ${var.release_channel}",
       "sudo hab svc load habitat/builder-api-proxy --group ${var.env} --bind http:builder-api.${var.env} --strategy at-once --url ${var.bldr_url} --channel ${var.release_channel}",
       "sudo hab svc load core/sumologic --group ${var.env} --strategy at-once --url ${var.bldr_url} --channel ${var.release_channel}",
     ]
