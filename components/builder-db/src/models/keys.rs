1005033cb9cee44d1932b00baf068b798118763c
Salim Alam <salam@chef.io>
2018-10-29 14:30:14 -0700

    Convert signing keys to direct DB
    Signed-off-by: Salim Alam <salam@chef.io>

--- components/builder-db/src/models/keys.rs
+++ components/builder-db/src/models/keys.rs
@@ -41,6 +41,40 @@ pub struct OriginPrivateEncryptionKey {
     pub updated_at: Option<NaiveDateTime>,
 }
 
+#[derive(Debug, Serialize, Deserialize, QueryableByName)]
+#[table_name = "origin_secret_keys"]
+pub struct OriginPrivateSigningKey {
+    #[serde(with = "db_id_format")]
+    pub id: i64,
+    #[serde(with = "db_id_format")]
+    pub origin_id: i64,
+    #[serde(with = "db_id_format")]
+    pub owner_id: i64,
+    pub name: String,
+    pub revision: String,
+    pub full_name: String,
+    pub body: Vec<u8>,
+    pub created_at: Option<NaiveDateTime>,
+    pub updated_at: Option<NaiveDateTime>,
+}
+
+#[derive(Debug, Serialize, Deserialize, QueryableByName)]
+#[table_name = "origin_public_keys"]
+pub struct OriginPublicSigningKey {
+    #[serde(with = "db_id_format")]
+    pub id: i64,
+    #[serde(with = "db_id_format")]
+    pub origin_id: i64,
+    #[serde(with = "db_id_format")]
+    pub owner_id: i64,
+    pub name: String,
+    pub revision: String,
+    pub full_name: String,
+    pub body: Vec<u8>,
+    pub created_at: Option<NaiveDateTime>,
+    pub updated_at: Option<NaiveDateTime>,
+}
+
 #[derive(Insertable)]
 #[table_name = "origin_public_encryption_keys"]
 pub struct NewOriginPublicEncryptionKey<'a> {
@@ -54,6 +88,26 @@ pub struct NewOriginPublicEncryptionKey<'a> {
 #[derive(Insertable)]
 #[table_name = "origin_private_encryption_keys"]
 pub struct NewOriginPrivateEncryptionKey<'a> {
+    pub owner_id: i64,
+    pub origin_id: i64,
+    pub name: &'a str,
+    pub revision: &'a str,
+    pub body: &'a [u8],
+}
+
+#[derive(Insertable)]
+#[table_name = "origin_secret_keys"]
+pub struct NewOriginPrivateSigningKey<'a> {
+    pub owner_id: i64,
+    pub origin_id: i64,
+    pub name: &'a str,
+    pub revision: &'a str,
+    pub body: &'a [u8],
+}
+
+#[derive(Insertable)]
+#[table_name = "origin_public_keys"]
+pub struct NewOriginPublicSigningKey<'a> {
     pub owner_id: i64,
     pub origin_id: i64,
     pub name: &'a str,
@@ -123,5 +177,68 @@ impl OriginPrivateEncryptionKey {
         .bind::<Text, _>(full_name)
         .bind::<Binary, _>(req.body)
         .get_result(conn)
+    }
+}
+
+impl OriginPublicSigningKey {
+    pub fn get(
+        origin: &str,
+        revision: &str,
+        conn: &PgConnection,
+    ) -> QueryResult<OriginPublicSigningKey> {
+        diesel::sql_query("select * from get_origin_public_key_v1($1, $2)")
+            .bind::<Text, _>(origin)
+            .bind::<Text, _>(revision)
+            .get_result(conn)
+    }
+
+    pub fn create(
+        req: &NewOriginPublicSigningKey,
+        conn: &PgConnection,
+    ) -> QueryResult<OriginPublicSigningKey> {
+        let full_name = format!("{}-{}", req.name, req.revision);
+        diesel::sql_query("select * from insert_origin_public_key_v1($1, $2, $3, $4, $5, $6)")
+            .bind::<BigInt, _>(req.origin_id)
+            .bind::<BigInt, _>(req.owner_id)
+            .bind::<Text, _>(req.name)
+            .bind::<Text, _>(req.revision)
+            .bind::<Text, _>(full_name)
+            .bind::<Binary, _>(req.body)
+            .get_result(conn)
+    }
+
+    pub fn latest(origin: &str, conn: &PgConnection) -> QueryResult<OriginPublicSigningKey> {
+        diesel::sql_query("select * from get_origin_public_key_latest_v1($1)")
+            .bind::<Text, _>(origin)
+            .get_result(conn)
+    }
+
+    pub fn list(origin_id: u64, conn: &PgConnection) -> QueryResult<Vec<OriginPublicSigningKey>> {
+        diesel::sql_query("select * from get_origin_public_keys_for_origin_v1($1)")
+            .bind::<BigInt, _>(origin_id as i64)
+            .get_results(conn)
+    }
+}
+
+impl OriginPrivateSigningKey {
+    pub fn get(origin: &str, conn: &PgConnection) -> QueryResult<OriginPrivateSigningKey> {
+        diesel::sql_query("select * from get_origin_secret_key_v1($1)")
+            .bind::<Text, _>(origin)
+            .get_result(conn)
+    }
+
+    pub fn create(
+        req: &NewOriginPrivateSigningKey,
+        conn: &PgConnection,
+    ) -> QueryResult<OriginPrivateSigningKey> {
+        let full_name = format!("{}-{}", req.name, req.revision);
+        diesel::sql_query("select * from insert_origin_secret_key_v1($1, $2, $3, $4, $5, $6)")
+            .bind::<BigInt, _>(req.origin_id)
+            .bind::<BigInt, _>(req.owner_id)
+            .bind::<Text, _>(req.name)
+            .bind::<Text, _>(req.revision)
+            .bind::<Text, _>(full_name)
+            .bind::<Binary, _>(req.body)
+            .get_result(conn)
     }
 }
