22c5915e7ab92d3c9c09b2f6f4b31826c416b6bb
Salim Alam <salam@chef.io>
2018-11-15 12:25:12 -0800

    Update packages view to only show distinct rows
    Signed-off-by: Salim Alam <salam@chef.io>

---
+++ components/builder-db/src/migrations/2018-11-15-202231_update_packages_list/up.sql
@@ -0,0 +1,24 @@
+CREATE OR REPLACE VIEW packages_with_channel_platform AS
+    SELECT DISTINCT
+        op.id,
+        op.owner_id,
+        op.name,
+        op.ident,
+        op.ident_array,
+        op.checksum,
+        op.manifest,
+        op.config,
+        op.target,
+        op.deps,
+        op.tdeps,
+        op.exposes,
+        op.visibility,
+        op.created_at,
+        op.updated_at,
+        op.origin,
+        array_agg(oc.name) OVER w AS channels,
+        array_agg(op.target) OVER w AS platforms
+    FROM origin_packages op
+    INNER JOIN origin_channel_packages AS ocp ON op.id = ocp.package_id
+    INNER JOIN origin_channels AS oc ON oc.id = ocp.channel_id
+    WINDOW w AS (PARTITION BY op.ident);
