a2e3d3124eef4f3f5c7b0543eef9da4e02c0ff9b
Jon Bauman <5906042+baumanj@users.noreply.github.com>
2019-04-10 13:38:15 -0400

    Add dummy files for generated code to avoid error in rustfmt
    Signed-off-by: Jon Bauman <5906042+baumanj@users.noreply.github.com>

--- support/ci/rustfmt.sh
+++ support/ci/rustfmt.sh
@@ -9,6 +9,19 @@ toolchain=$(get_current_toolchain)
 
 install_rustup
 install_rustfmt "$toolchain"
+
+# A hack to avoid rustfmt choking on nonexistent generated files
+# These can't be ignored because the error is rustc-level, before rustfmt
+# Alternatively, we could do a `cargo build` first, but that'd take awhile.
+# Because rustfmt requires an empty file to still have a newline, we can't
+# just use `touch` here.
+for generated_file in components/builder-protocol/src/message/{jobsrv,net,originsrv}.rs; do
+	if [[ ! -s "$generated_file" ]]; then
+		echo > "$generated_file"
+	fi
+done
+
+touch components/builder-protocol/src/message/{jobsrv,net,originsrv}.rs
 cargo_fmt="cargo +$toolchain fmt --all -- --check"
 echo "--- :rust: Running cargo fmt command: $cargo_fmt"
 $cargo_fmt
