9ca8e498384c53e67364de2c8560649e27b6b206
Scott Macfarlane <smacfarlane@chef.io>
2019-09-19 09:53:53 -0700

    Migrate api tests to verify pipeline
    Signed-off-by: Scott Macfarlane <smacfarlane@chef.io>

---
+++ .expeditor/scripts/verify/builder-api-functional.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -euo pipefail
+
+echo "--- Generating signing key"
+hab origin key generate $HAB_ORIGIN
+
+echo "--- Updating .studiorc" 
+cat .expeditor/templates/studiorc >> .studiorc
+
+echo "--- Copying habitat-env"
+cp .secrets/habitat-env.sample .secrets/habitat-env
+
+echo "--- Entering studio"
+env HAB_INTERACTIVE=true \
+    HAB_STUDIO_SUP=false \
+    hab studio enter
