725e4043a556af332ed849e39ea40e8df366bf05
Scott Macfarlane <smacfarlane@chef.io>
2020-04-08 16:57:30 -0700

    Get latest packages from database
    Signed-off-by: Scott Macfarlane <smacfarlane@chef.io>

--- components/builder-graph/src/data_store.rs
+++ components/builder-graph/src/data_store.rs
@@ -12,8 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-use std::{str::FromStr,
-          sync::Arc};
+use std::sync::Arc;
 
 use crate::hab_core::package::{PackageIdent,
                                PackageTarget};
@@ -26,10 +25,15 @@ use crate::{config::Config,
             db::{models::package::{GetLatestPackage,
                                    Package,
                                    PackageVisibility},
+                 schema::{channel::{origin_channel_packages,
+                                    origin_channels},
+                          package::origin_packages},
                  DbPool},
+            diesel::{ExpressionMethods,
+                     QueryDsl,
+                     RunQueryDsl},
             error::{Error,
                     Result}};
-
 // DataStore inherits Send + Sync by virtue of having only one member, the pool itself.
 #[derive(Clone)]
 pub struct DataStore {
@@ -93,88 +97,22 @@ impl DataStore {
     }
 
     pub fn get_origin_channel_latest(&self,
-                                     _origin: &str,
-                                     _channel: &str)
+                                     origin: &str,
+                                     channel: &str,
+                                     target: PackageTarget)
                                      -> Result<Vec<PackageIdent>> {
-        // I want something like this query but haven't gotten it to work in diesel
-        //
-        // SELECT distinct on(op.name)
-        //     op.ident
-        //    FROM origin_packages op
-        //      JOIN origin_channel_packages ocp ON op.id = ocp.package_id
-        //      JOIN origin_channels oc ON oc.id = ocp.channel_id
-        //    WHERE
-        //      op.origin = 'core' AND
-        //      oc.name = 'stable'
-        //    ORDER BY
-        //      op.name,
-        //      regexp_matches(op.ident_array[3], '([\d\.]*\d+)(.+)?'::text) desc,
-        //      op.ident_array[4] desc;
+        let conn = self.pool.get_conn()?;
 
-        // for now, hardcoding latest of gang of 42 should be enough to get us started
-        //
-        let gang_of_42 = "core/acl/2.2.53/20190115012136
-core/attr/2.4.48/20190115012129
-core/bash/4.4.19/20190115012619
-core/bash-completion/2.8/20190131162722
-core/bash-static/4.4.19/20190402151504
-core/binutils/2.31.1/20190115003743
-core/bison/3.0.5/20190115012441
-core/bison2/2.7.1/20190115161755
-core/bzip2/1.0.6/20190725144604
-core/bzip2-musl/1.0.6/20190115014608
-core/coreutils/8.30/20190115012313
-core/coreutils-static/8.30/20190115183917
-core/db/5.3.28/20190115012845
-core/dbus/1.13.8/20190201184338
-core/dejagnu/1.6.1/20190115014148
-core/diffutils/3.6/20190115013221
-core/expect/5.45.4/20190115014137
-core/file/5.34/20190115003731
-core/filebeat/7.2.0/20190626161648
-core/findutils/4.6.0/20190115013303
-core/gawk/4.2.1/20190115012752
-core/gcc/8.2.0/20190115004042
-core/gcc-libs/8.2.0/20190115011926
-core/gdbm/1.17/20190115012826
-core/gettext/0.19.8/20190115013412
-core/glibc/2.27/20190115002733
-core/gmp/6.1.2/20190115003943
-core/grep/3.1/20190115012541
-core/gzip/1.9/20190115013612
-core/iana-etc/2.30/20190115013006
-core/inetutils/1.9.4/20190115012922
-core/less/530/20190115013008
-core/lessmsi/1.6.1/20190131231730
-core/libcap/2.25/20190115012150
-core/libcap-ng/0.7.8/20190117153131
-core/libmpc/1.1.0/20190115004027
-core/linux-headers/4.3/20170513200956
-core/linux-headers-musl/3.12.6-6/20190115014537
-core/m4/1.4.18/20190115003920
-core/make/4.2.1/20190115013626
-core/mpfr/4.0.1/20190115004008
-core/ncurses/6.1/20190115012027
-core/ncurses5/6.1/20190116151445
-core/patch/2.7.6/20190115013636
-core/patchelf/0.9/20190115011946
-core/pcre/8.42/20190115012526
-core/perl/5.28.0/20190115013014
-core/procps-ng/3.3.15/20190115012258
-core/readline/7.0.3/20190115012607
-core/readline6/6.3.8/20190117175148
-core/sed/4.5/20190115012152
-core/tcl/8.6.8/20190115013933
-core/texinfo/6.5/20190115013702
-core/xz/5.2.4/20190115013348
-core/xz-musl/5.2.4/20190115014612
-core/zlib/1.2.8/20170513201911
-core/zlib-musl/1.2.8/20180310002650
-";
-        let idents: Vec<PackageIdent> =
-            gang_of_42.lines()
-                      .map(|x| PackageIdent::from_str(x.trim()).unwrap())
-                      .collect();
+        let query =
+            origin_packages::table.inner_join(origin_channel_packages::table.inner_join(origin_channels::table))
+                                  .distinct_on((origin_packages::name, origin_packages::target))
+                                  .select(origin_packages::ident)
+                                  .filter(origin_channels::name.eq(&channel))
+                                  .filter(origin_packages::origin.eq(&origin))
+                                  .filter(origin_packages::target.eq(target.to_string()));
+
+        let result: Vec<BuilderPackageIdent> = query.get_results(&conn).unwrap();
+        let idents = result.into_iter().map(|r| r.0).collect();
         Ok(idents)
     }
 }
