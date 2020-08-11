f06534549749e52c81ca6bfa7db320627fe8cc2b
mwrock <matt@mattwrock.com>
2020-08-10 17:13:02 -0700

    enable ssh access to windows worker
    Signed-off-by: mwrock <matt@mattwrock.com>

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -164,3 +164,7 @@ variable "enabled_features" {
   type    = list(string)
   default = []
 }
+
+variable "aws_public_key" {
+  description = "File path to the public key to use for authentication to windows builder"
+}
