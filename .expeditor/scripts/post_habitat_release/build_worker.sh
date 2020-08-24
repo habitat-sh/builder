c0278c9d8f9cc65bc73fe6c08715d3d5dc4a6302
Scott Macfarlane <smacfarlane@chef.io>
2020-08-24 14:09:51 -0700

    Set the hab_binary variable
    Signed-off-by: Scott Macfarlane <smacfarlane@chef.io>

--- .expeditor/scripts/post_habitat_release/build_worker.sh
+++ .expeditor/scripts/post_habitat_release/build_worker.sh
@@ -21,6 +21,12 @@ export HAB_BLDR_URL="${PIPELINE_HAB_BLDR_URL}"
 # the container we happen to be running in.
 curlbash_hab "${BUILD_PKG_TARGET}"
 
+# Explicitly set what hab binary we want to use, to avoid any potential conflicts
+# with $PATH and provided cli.
+# FAILURE CASE: If we change the default install location, this will possibly fail, or 
+#               use an incorrect version of the cli.
+hab_binary="/bin/hab"
+
 import_keys "${HAB_ORIGIN}"
 
 echo "--- :habicat: Building builder-worker using $hab_binary "
