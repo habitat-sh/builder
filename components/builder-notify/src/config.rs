b4bc759ea46bd4c269778dbf891ff80cad871d04
Phani Sajja <psajja@progress.com>
2021-03-17 22:37:15 +0530

    Update config file to read webhook config
    Signed-off-by: Phani Sajja <psajja@progress.com>

--- components/builder-notify/src/config.rs
+++ components/builder-notify/src/config.rs
@@ -6,8 +6,21 @@ use habitat_builder_events::connection::EventConfig;
 #[serde(default)]
 pub struct Config {
     pub eventbus: EventConfig,
+    pub hub:      HubCfg,
 }
 
 impl ConfigFile for Config {
     type Error = Error;
+}
+
+#[derive(Clone, Debug, Default, Deserialize)]
+#[serde(default)]
+pub struct HubCfg {
+    pub webhooks: Vec<WebhookCfg>,
+}
+
+#[derive(Clone, Debug, Default, Deserialize)]
+#[serde(default)]
+pub struct WebhookCfg {
+    pub endpoint: String,
 }
