2e833a0061c3137505bbc003f14a63db4e18519b
Phani Sajja <psajja@progress.com>
2021-04-05 20:22:03 +0530

    [PR Feedback] remove extra decoration
    Signed-off-by: Phani Sajja <psajja@progress.com>

--- components/builder-notify/src/main.rs
+++ components/builder-notify/src/main.rs
@@ -1,9 +1,9 @@
 use habitat_builder_notify::{cli::Notify,
                              server};
-use std::{fmt,
-          process};
 use structopt::StructOpt;
 
+#[macro_use]
+extern crate log;
 extern crate env_logger;
 
 #[actix_rt::main]
@@ -11,17 +11,10 @@ async fn main() {
     env_logger::init();
     match Notify::from_args() {
         Notify::Run { config } => {
-            match server::run(config).await {
-                Ok(_) => std::process::exit(0),
-                Err(e) => exit_with(e, 1),
+            if let Err(e) = server::run(config).await {
+                error!("Error in 'builder-notify' service {}", e);
+                std::process::exit(1)
             }
         }
     }
-}
-
-fn exit_with<T>(err: T, code: i32)
-    where T: fmt::Display
-{
-    println!("Error in 'builder-notify' service {}", err);
-    process::exit(code)
 }
