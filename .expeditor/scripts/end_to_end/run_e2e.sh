7a2ca0dfa590bc2be0c1510e892202b0dd1849f8
pozsgaic <pozsgai@progress.com>
2022-06-10 19:09:13 +0000

    shellcheck
    Signed-off-by: pozsgaic <pozsgai@progress.com>

--- .expeditor/scripts/end_to_end/run_e2e.sh
+++ .expeditor/scripts/end_to_end/run_e2e.sh
@@ -4,11 +4,12 @@ set -eou pipefail
 umask 0022 
 
 #  Always run from builder root directory.
-export ROOT_DIR=$(pwd)
+export ROOT_DIR
+ROOT_DIR=$(pwd)
 WORK_DIR=${ROOT_DIR}/test/end-to-end/worker-test
 
-source ${WORK_DIR}/bldr-end-to-end.env
-source ${WORK_DIR}/shared.sh
+source "${WORK_DIR}"/bldr-end-to-end.env
+source "${WORK_DIR}"/shared.sh
 
 export HAB_FUNC_TEST=1
 
@@ -21,7 +22,7 @@ start_jobsrv
 apply_db_password
 sleep 5
 
-sudo cp ${WORK_DIR}/builder-github-app.pem /hab/svc/builder-api/files
+sudo cp "${WORK_DIR}"/builder-github-app.pem /hab/svc/builder-api/files
 
 echo "start_worker"
 start_worker
@@ -35,7 +36,7 @@ EOT
 hab config apply builder-worker.default "$(date +%s)" /tmp/builder_worker.toml
 sleep 3
 echo "worker config updated"
-sudo cp ${WORK_DIR}/builder-github-app.pem /hab/svc/builder-worker/files
+sudo cp "${WORK_DIR}"/builder-github-app.pem /hab/svc/builder-worker/files
 
 #  HACK  To get builder-worker HAB_FUNC_TEST changes 
 #hab svc stop habitat/builder-worker
@@ -54,8 +55,8 @@ done
 echo "Services have started - continuing with tests"
 
 #  Need to test if we need this file in this location and remove if not.
-sudo cp ${WORK_DIR}/fixtures/neurosis-20171211220037.pub /hab/svc/builder-worker/files
-sudo cp ${WORK_DIR}/fixtures/neurosis-20171211220037.sig.key /hab/svc/builder-worker/files
+sudo cp "${WORK_DIR}"/fixtures/neurosis-20171211220037.pub /hab/svc/builder-worker/files
+sudo cp "${WORK_DIR}"/fixtures/neurosis-20171211220037.sig.key /hab/svc/builder-worker/files
 
 sudo cp /hab/svc/builder-api/files/bldr-*.pub /hab/svc/builder-worker/files
 sudo cp /hab/svc/builder-api/files/bldr-*.box.key /hab/svc/builder-worker/files
