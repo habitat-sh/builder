50146aede0f6ef1465cd7c8e8458dbade4889819
sougata-progress <sougatab@progress.com>
2024-09-19 12:03:15 +0000

    Updated build script and plan script for builder-api
    Signed-off-by: sougata-progress <sougatab@progress.com>

--- build.sh
+++ build.sh
@@ -14,25 +14,24 @@ if [[ "$component" == "builder-jobsrv" || "$component" == "builder-worker" ]]; t
 fi
 
 # Accept hab license
-sudo hab license accept
-sudo hab pkg install core/rust/"$toolchain"
-sudo hab pkg install core/libarchive
-sudo hab pkg install core/openssl
-sudo hab pkg install core/zeromq
-sudo hab pkg install core/pkg-config
-sudo hab pkg install core/protobuf
-sudo hab pkg install core/postgresql
-sudo hab pkg install core/cmake
+sudo hab pkg install core/rust/"$toolchain" --channel LTS-2024
+sudo hab pkg install core/libarchive --channel LTS-2024
+sudo hab pkg install core/openssl --channel LTS-2024
+sudo hab pkg install core/zeromq --channel LTS-2024
+sudo hab pkg install core/pkg-config --channel LTS-2024
+sudo hab pkg install core/protobuf --channel LTS-2024
+sudo hab pkg install core/postgresql15 --channel LTS-2024
+sudo hab pkg install core/cmake --channel LTS-2024
 # It is important NOT to use a vendored openssl from openssl-sys
 # pg-sys does not use openssl-sys. So for components that use
 # diesel's postgres feature, you wil end up with 2 versions of openssl
 # which can lead to segmentation faults when connecting to postgres
 export OPENSSL_NO_VENDOR=1
 export LD_RUN_PATH
-LD_RUN_PATH="$(hab pkg path core/glibc)/lib:$(hab pkg path core/gcc-libs)/lib:$(hab pkg path core/openssl)/lib:$(hab pkg path core/postgresql)/lib:$(hab pkg path core/zeromq)/lib:$(hab pkg path core/libarchive)/lib"
+LD_RUN_PATH="$(hab pkg path core/glibc)/lib:$(hab pkg path core/gcc-libs)/lib:$(hab pkg path core/openssl)/lib:$(hab pkg path core/postgresql15)/lib:$(hab pkg path core/zeromq)/lib:$(hab pkg path core/libarchive)/lib"
 export PKG_CONFIG_PATH
-PKG_CONFIG_PATH="$(hab pkg path core/zeromq)/lib/pkgconfig:$(hab pkg path core/libarchive)/lib/pkgconfig:$(hab pkg path core/postgresql)/lib/pkgconfig:$(hab pkg path core/openssl)/lib/pkgconfig"
-eval "$(hab pkg env core/rust/"$toolchain"):$(hab pkg path core/protobuf)/bin:$(hab pkg path core/pkg-config)/bin:$(hab pkg path core/postgresql)/bin:$(hab pkg path core/cmake)/bin:$PATH"
+PKG_CONFIG_PATH="$(hab pkg path core/zeromq)/lib/pkgconfig:$(hab pkg path core/libarchive)/lib/pkgconfig:$(hab pkg path core/postgresql15)/lib/pkgconfig:$(hab pkg path core/openssl)/lib/pkgconfig"
+eval "$(hab pkg env core/rust/"$toolchain"):$(hab pkg path core/protobuf)/bin:$(hab pkg path core/pkg-config)/bin:$(hab pkg path core/postgresql15)/bin:$(hab pkg path core/cmake)/bin:$PATH"
 
 cd "components/$component"
 cargo build
