60314ad433725a6c51e0bad6a37745c61d3d9101
Jason Heath <jh@jasonheath.com>
2025-01-16 18:42:37 -0500

    Upgrade builder-datastore to use core/postgresql17
    - Adds components/builder-datastore/hooks/install
    - Removes components/builder-datastore/hooks/init
    - Updates components/builder-datastore/hooks/run
    - Uses core/postgresql17 as the package ident everywhere
    - Removes unused keys from components/builder-api/habitat/default.toml
    - Reworks components/builder-api/habitat/hooks/init
    - Updates or removes postgres conf items from postgresql.conf and default.toml
      files as appropriate
    - Updates changed items from postgresql.conf and applicabale default.toml files
    - Removes ext_semver_version from datastore plan
    
    Signed-off-by: Jason Heath <jason.heath@progress.com>

--- components/builder-datastore/plan.sh
+++ components/builder-datastore/plan.sh
@@ -3,31 +3,14 @@
 
 pkg_origin=habitat
 pkg_name=builder-datastore
-pkg_internal_version=11.2
-pkg_internal_name=postgresql11
-pkg_description="Datastore service for a Habitat Builder service"
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
-pkg_license=("PostgreSQL")
-pkg_source="https://ftp.postgresql.org/pub/source/v${pkg_internal_version}/postgresql-${pkg_internal_version}.tar.bz2"
-pkg_shasum="2676b9ce09c21978032070b6794696e0aa5a476e3d21d60afc036dc0a9c09405"
-pkg_dirname="postgresql-${pkg_internal_version}"
+pkg_license=('Apache-2.0')
 
-pkg_deps=(
-  core/bash
-  core/glibc
-  core/openssl
-  core/perl
-  core/readline
-  core/zlib
-  core/libossp-uuid
-)
+pkg_description="Datastore service for a Habitat Builder service"
 
-pkg_build_deps=(
-  core/coreutils
-  core/gcc
-  core/make
-  core/git
-)
+pkg_deps=(core/postgresql17)
+
+pkg_build_deps=(core/git)
 
 pkg_bin_dirs=(bin)
 pkg_include_dirs=(include)
@@ -43,67 +26,15 @@ pkg_version() {
   echo "$(($(git rev-list HEAD --count) + 5000))"
 }
 
-ext_semver_version=0.17.0
-ext_semver_source=https://github.com/theory/pg-semver/archive/v${ext_semver_version}.tar.gz
-ext_semver_filename=pg-semver-${ext_semver_version}.tar.gz
-ext_semver_shasum=031046695b143eb545a2856c5d139ebf61ae4e2f68cccb1f21b700ce65d0cd60
-
 do_before() {
   git config --global --add safe.directory /src
   update_pkg_version
-  ext_semver_dirname="pg-semver-${ext_semver_version}"
-  ext_semver_cache_path="$HAB_CACHE_SRC_PATH/${ext_semver_dirname}"
-}
-
-do_download() {
-  do_default_download
-  download_file $ext_semver_source $ext_semver_filename $ext_semver_shasum
-}
-
-do_verify() {
-  do_default_verify
-  verify_file $ext_semver_filename $ext_semver_shasum
-}
-
-do_clean() {
-  do_default_clean
-  rm -rf "$ext_semver_cache_path"
-}
-
-do_unpack() {
-  do_default_unpack
-  unpack_file $ext_semver_filename
 }
 
 do_build() {
-  # shellcheck disable=2154
-  # ld manpage: "If -rpath is not used when linking an ELF
-  # executable, the contents of the environment variable LD_RUN_PATH
-  # will be used if it is defined"
-  ./configure --disable-rpath \
-    --with-openssl \
-    --prefix="$pkg_prefix" \
-    --with-uuid=ossp \
-    --with-includes="$LD_INCLUDE_PATH" \
-    --with-libraries="$LD_LIBRARY_PATH" \
-    --sysconfdir="$pkg_svc_config_path" \
-    --localstatedir="$pkg_svc_var_path"
-  make world
-
-  # semver can't be built until after postgresql is installed to $pkg_prefix
+  return 0
 }
 
 do_install() {
-  make install-world
-
-  # make and install semver extension
-  export PATH="${PATH}:${pkg_prefix}/bin"
-  build_line "Added postgresql binaries to PATH: ${pkg_prefix}/bin"
-
-  pushd "$ext_semver_cache_path" >/dev/null || exit
-  build_line "Building ${ext_semver_dirname}"
-  make
-  build_line "Installing ${ext_semver_dirname}"
-  make install
-  popd >/dev/null || exit
+  return 0
 }
