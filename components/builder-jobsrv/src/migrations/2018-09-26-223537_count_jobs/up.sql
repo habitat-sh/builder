c00b99ac2493817593e396f81270f48cf998235f
Jason Heath <jh@jasonheath.com>
2025-01-17 09:40:08 -0500

    Removes obsolete components and habitat-dev plans
    Code removal was equivalent to the following:
    - git rm -r components/builder-graph
    - git rm -r components/builder-jobsrv
    - git rm -r components/builder-worker
    - git rm -r components/builder-api/habitat-dev
    - git rm -r components/builder-api-proxy/habitat-dev
    
    After that the top level Cargo.toml was updated to remove the graph,
    jobsrv, and worker components which dropped a lot of dependencies
    and restored basic cargo functionality.
    
    Signed-off-by: Jason Heath <jh@jasonheath.com>

--- components/builder-jobsrv/src/migrations/2018-09-26-223537_count_jobs/up.sql
+++
@@ -1,7 +0,0 @@
-CREATE OR REPLACE FUNCTION count_jobs_v1(in_job_state text) RETURNS bigint
-    LANGUAGE plpgsql STABLE
-    AS $$
-  BEGIN
-    RETURN COUNT(*) FROM jobs WHERE job_state = in_job_state;
-  END
-$$;
