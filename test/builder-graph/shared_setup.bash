700d9f36bd9414c3e85988945df20bd967ccdf92
Jason Heath <jh@jasonheath.com>
2025-01-28 08:22:55 -0500

    Removes test/builder-graph/
    Signed-off-by: Jason Heath <jh@jasonheath.com>

--- test/builder-graph/shared_setup.bash
+++
@@ -1,6 +0,0 @@
-builder_graph() {
-    "${BLDR_GRAPH_PATH:-"$BATS_TEST_DIRNAME/../../target/debug/bldr-graph"}" "${@}"
-}
-
-# shellcheck disable=SC2034
-packages_db="$BATS_TEST_DIRNAME/fixtures/db-core-2022-08-22.json"
