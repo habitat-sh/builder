bf4e20fa6c64fe4e4b588d6d0978f794f19629de
Jason Heath <jh@jasonheath.com>
2025-02-03 10:39:51 -0500

    Restores habitat-dev plan files and shellchecks/shfmts the rest
    Signed-off-by: Jason Heath <jh@jasonheath.com>

--- components/builder-api-proxy/habitat/plan.sh
+++ components/builder-api-proxy/habitat/plan.sh
@@ -1,14 +1,20 @@
+#!/bin/bash
 # shellcheck disable=SC2034
+
 pkg_origin=habitat
 pkg_name=builder-api-proxy
 pkg_description="HTTP Proxy service fronting the Habitat Builder API service"
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 pkg_license=("Apache-2.0")
-pkg_deps=(core/nginx core/curl core/coreutils)
+pkg_deps=(
+  core/nginx
+  core/curl
+  core/coreutils
+)
 
 pkg_build_deps=(
-  core/node/"$(cat "$PLAN_CONTEXT/../../builder-web/.nvmrc")"
   core/git
+  core/node/"$(cat "$PLAN_CONTEXT/../../builder-web/.nvmrc")"
   core/tar
 )
 pkg_svc_user="root"
@@ -36,14 +42,17 @@ do_before() {
 }
 
 do_unpack() {
-  pushd "$PLAN_CONTEXT/../../builder-web" > /dev/null || exit
-  { git ls-files; git ls-files --exclude-standard --others; } \
-  | _tar_pipe_app_cp_to "${HAB_CACHE_SRC_PATH}"
-  popd > /dev/null || exit
+  pushd "$PLAN_CONTEXT/../../builder-web" >/dev/null || exit
+  {
+    git ls-files
+    git ls-files --exclude-standard --others
+  } |
+    _tar_pipe_app_cp_to "${HAB_CACHE_SRC_PATH}"
+  popd >/dev/null || exit
 }
 
 do_build() {
-  pushd "$HAB_CACHE_SRC_PATH" > /dev/null || exit
+  pushd "$HAB_CACHE_SRC_PATH" >/dev/null || exit
   export HOME=$HAB_CACHE_SRC_PATH
   export PATH=./node_modules/.bin:$PATH
   npm install
@@ -56,7 +65,7 @@ do_build() {
   npm run dist -- "${pkg_prefix: -14}"
 
   rm -rf dist/node_modules
-  popd > /dev/null || exit
+  popd >/dev/null || exit
 }
 
 do_install() {
@@ -68,15 +77,15 @@ _tar_pipe_app_cp_to() {
   dst_path="$1"
   tar="$(pkg_path_for tar)/bin/tar"
   "$tar" -cp \
-  --owner=root:0 \
-  --group=root:0 \
-  --no-xattrs \
-  --exclude-backups \
-  --exclude-vcs \
-  --exclude='habitat' \
-  --files-from=- \
-  -f - \
-  | "$tar" -x \
-  -C "$dst_path" \
-  -f -
+    --owner=root:0 \
+    --group=root:0 \
+    --no-xattrs \
+    --exclude-backups \
+    --exclude-vcs \
+    --exclude='habitat' \
+    --files-from=- \
+    -f - |
+    "$tar" -x \
+      -C "$dst_path" \
+      -f -
 }
