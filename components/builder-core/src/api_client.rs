e388f2f417ea43ca24e0e2de0cee0d4ef44bc107
Jason Heath <jh@jasonheath.com>
2025-02-14 10:04:10 -0500

    'Top Down' removal from components/builder-api
    Signed-off-by: Jason Heath <jh@jasonheath.com>

--- components/builder-core/src/api_client.rs
+++ components/builder-core/src/api_client.rs
@@ -12,35 +12,21 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-use std::{collections::HashMap,
-          fs,
-          path::{Path,
-                 PathBuf}};
+use std::collections::HashMap;
 
-use rand::{distributions::Alphanumeric,
-           thread_rng,
-           Rng};
 
 use reqwest::{header::HeaderMap,
-              Body,
               Response,
               StatusCode};
-
-use futures::stream::StreamExt;
-use tokio::io::AsyncWriteExt;
 
 use crate::{error::{Error,
                     Result},
             hab_core::{package::{self,
                                  Identifiable,
-                                 PackageArchive,
                                  PackageTarget},
-                       ChannelIdent}};
+                       ChannelIdent}, http_client::{ACCEPT_APPLICATION_JSON, USER_AGENT_BLDR}};
 
-use crate::http_client::{HttpClient,
-                         ACCEPT_APPLICATION_JSON,
-                         USER_AGENT_BLDR,
-                         XFILENAME};
+use crate::http_client::HttpClient;
 
 #[derive(Clone, Deserialize)]
 pub struct PackageIdent {
@@ -87,166 +73,6 @@ impl ApiClient {
 
         Ok(ApiClient { inner: HttpClient::new(url, headers)?,
                        url:   url.to_owned(), })
-    }
-
-    pub async fn show_package<I>(&self,
-                                 package: &I,
-                                 channel: &ChannelIdent,
-                                 target: &str,
-                                 token: Option<&str>)
-                                 -> Result<Package>
-        where I: Identifiable
-    {
-        let mut url = channel_package_path(channel, package);
-
-        if !package.fully_qualified() {
-            url.push_str("/latest");
-        }
-
-        let url_path = format!("{}/v1/{}", self.url, url);
-        let mut query = HashMap::new();
-        query.insert("target", target);
-
-        let mut request = self.inner.get(&url_path).query(&query);
-
-        if let Some(token) = token {
-            request = request.bearer_auth(token);
-        }
-
-        let resp = request.send().await.map_err(Error::HttpClient)?;
-
-        if resp.status() != StatusCode::OK {
-            return Err(err_from_response(resp).await);
-        }
-
-        let body = resp.text().await?;
-        debug!("Body: {:?}", body);
-
-        let package: Package =
-            serde_json::from_str::<Package>(&body).map_err(Error::Serialization)?;
-        Ok(package)
-    }
-
-    pub async fn fetch_package<I, P>(&self,
-                                     ident: &I,
-                                     target: &str,
-                                     dst_path: &P,
-                                     token: Option<&str>)
-                                     -> Result<PackageArchive>
-        where P: AsRef<Path> + ?Sized,
-              I: Identifiable
-    {
-        let url = &package_download(ident);
-
-        let mut qparams = HashMap::new();
-        qparams.insert("target", target);
-
-        match self.download(url, &qparams, dst_path.as_ref(), token).await {
-            Ok(file) => Ok(PackageArchive::new(file)?),
-            Err(e) => Err(e),
-        }
-    }
-
-    async fn download(&self,
-                      url: &str,
-                      qparams: &HashMap<&str, &str>,
-                      dst_path: &Path,
-                      token: Option<&str>)
-                      -> Result<PathBuf> {
-        let url_path = format!("{}/v1/{}", self.url, url);
-
-        let mut request = self.inner.get(&url_path).query(&qparams);
-
-        if let Some(token) = token {
-            request = request.bearer_auth(token);
-        }
-
-        let resp = request.send().await?;
-
-        debug!("Response: {:?}", resp);
-
-        if resp.status() != StatusCode::OK {
-            return Err(err_from_response(resp).await);
-        }
-
-        fs::create_dir_all(dst_path).map_err(Error::IO)?;
-
-        let file_name = match resp.headers().get(XFILENAME.clone()) {
-            Some(f) => f.to_str().expect("X-Filename header exists"),
-            None => return Err(Error::BadResponse),
-        };
-
-        let tmp_file_path = dst_path.join(format!("{}.tmp-{}",
-                                                  file_name,
-                                                  thread_rng().sample_iter(&Alphanumeric)
-                                                              .take(8)
-                                                              .map(char::from)
-                                                              .collect::<String>()));
-
-        let dst_file_path = dst_path.join(file_name);
-
-        debug!("Writing to {}", &tmp_file_path.display());
-        let mut f = tokio::fs::File::create(&tmp_file_path).await
-                                                           .map_err(Error::IO)?;
-
-        let mut stream = resp.bytes_stream();
-        while let Some(chunk) = stream.next().await {
-            f.write_all(&chunk?).await.map_err(Error::IO)?;
-        }
-
-        debug!("Moving {} to {}",
-               &tmp_file_path.display(),
-               &dst_file_path.display());
-        fs::rename(&tmp_file_path, &dst_file_path).map_err(Error::IO)?;
-        Ok(dst_file_path)
-    }
-
-    pub async fn x_put_package(&self, pa: &mut PackageArchive, token: &str) -> Result<()> {
-        let checksum = pa.checksum()?;
-        let ident = pa.ident()?;
-        let target = pa.target()?;
-
-        let url_path = format!("{}/v1/{}", self.url, package_path(&ident));
-
-        let mut qparams: HashMap<&str, &str> = HashMap::new();
-        qparams.insert("checksum", &checksum);
-        qparams.insert("target", &target);
-        qparams.insert("builder", "");
-
-        debug!("Reading from {}", &pa.path.display());
-
-        let body: Body = tokio::fs::read(&pa.path).await.map_err(Error::IO)?.into();
-
-        let resp = self.inner
-                       .post(&url_path)
-                       .query(&qparams)
-                       .body(body)
-                       .bearer_auth(token)
-                       .send()
-                       .await
-                       .map_err(Error::HttpClient)?;
-
-        match resp.status() {
-            StatusCode::CREATED | StatusCode::CONFLICT => (), // Conflict means package already
-            // uploaded - return Ok
-            _ => return Err(err_from_response(resp).await),
-        }
-
-        Ok(())
-    }
-
-    pub async fn fetch_origin_secret_key<P>(&self,
-                                            origin: &str,
-                                            token: &str,
-                                            dst_path: P)
-                                            -> Result<PathBuf>
-        where P: AsRef<Path>
-    {
-        self.download(&origin_secret_keys_latest(origin),
-                      &HashMap::new(),
-                      dst_path.as_ref(),
-                      Some(token))
-            .await
     }
 
     pub async fn create_channel(&self,
@@ -303,40 +129,6 @@ impl ApiClient {
 
         Ok(())
     }
-}
-
-fn channel_package_path<I>(channel: &ChannelIdent, package: &I) -> String
-    where I: Identifiable
-{
-    let mut path = format!("depot/channels/{}/{}/pkgs/{}",
-                           package.origin(),
-                           channel,
-                           package.name());
-    if let Some(version) = package.version() {
-        path.push('/');
-        path.push_str(version);
-        if let Some(release) = package.release() {
-            path.push('/');
-            path.push_str(release);
-        }
-    }
-    path
-}
-
-fn package_download<I>(package: &I) -> String
-    where I: Identifiable
-{
-    format!("{}/download", package_path(package))
-}
-
-fn package_path<I>(package: &I) -> String
-    where I: Identifiable
-{
-    format!("depot/pkgs/{}", package)
-}
-
-fn origin_secret_keys_latest(origin: &str) -> String {
-    format!("depot/origins/{}/secret_keys/latest", origin)
 }
 
 fn channel_package_promote<I>(channel: &ChannelIdent, package: &I) -> String
--- components/builder-core/src/logger.rs
+++ components/builder-core/src/logger.rs
@@ -16,12 +16,7 @@ use chrono::prelude::*;
 use std::{fs::{File,
                OpenOptions},
           io::Write,
-          path::Path,
-          time::Duration};
-
-use crate::protocol::jobsrv::{Job,
-                              JobGroup,
-                              JobGroupProject};
+          path::Path};
 
 pub struct Logger {
     file: File,
@@ -45,116 +40,6 @@ impl Logger {
         self.file
             .write_all(fmt_msg.as_bytes())
             .unwrap_or_else(|_| panic!("Logger unable to write to {:?}", self.file));
-    }
-
-    // Log format (fields are comma-separated)
-    //   Log entry datetime (Utc)
-    //   Entry type - G (group), J (job), P (project), W (worker), I (ident)
-    //   Id (group or job id)
-    //   State
-    //   Project name (for job or project)
-    //   Start datetime (Utc) (only for jobs)
-    //   End datetime (Utc) (only for jobs)
-    //   Start offset (offset from group creation, in seconds, only for jobs)
-    //   Duration (job duration, in seconds, only for jobs)
-    //   Error (if applicable)
-    pub fn log_ident(&mut self, ident: &str) {
-        let msg = format!("I,Started {}", ident);
-        self.log(&msg);
-    }
-
-    pub fn log_group(&mut self, group: &JobGroup) {
-        let msg = format!("G,{},{:?},", group.get_id(), group.get_state());
-        self.log(&msg);
-    }
-
-    pub fn log_group_project(&mut self, group: &JobGroup, project: &JobGroupProject) {
-        let msg = format!("P,{},{:?},{},",
-                          group.get_id(),
-                          project.get_state(),
-                          project.get_name());
-        self.log(&msg);
-    }
-
-    pub fn log_group_job(&mut self, group: &JobGroup, job: &Job) {
-        let suffix = if job.has_build_started_at() && job.has_build_finished_at() {
-            let start = job.get_build_started_at()
-                           .parse::<DateTime<Utc>>()
-                           .unwrap_or_else(|_| Utc::now());
-            let stop = job.get_build_finished_at()
-                          .parse::<DateTime<Utc>>()
-                          .unwrap_or_else(|_| Utc::now());
-            let group_start = group.get_created_at()
-                                   .parse::<DateTime<Utc>>()
-                                   .unwrap_or_else(|_| Utc::now());
-
-            let offset = start.signed_duration_since(group_start)
-                              .to_std()
-                              .unwrap_or_else(|_| Duration::from_secs(0))
-                              .as_secs_f64();
-            let duration = stop.signed_duration_since(start)
-                               .to_std()
-                               .unwrap_or_else(|_| Duration::from_secs(0))
-                               .as_secs_f64();
-
-            format!("{},{},{},{}",
-                    offset,
-                    duration,
-                    start.format("%Y-%m-%d %H:%M:%S"),
-                    stop.format("%Y-%m-%d %H:%M:%S"))
-        } else {
-            "".to_string()
-        };
-
-        let error = if job.has_error() {
-            format!("{:?}", job.get_error())
-        } else {
-            "".to_string()
-        };
-
-        let msg = format!("J,{},{},{:?},{},{},{},{}",
-                          job.get_owner_id(),
-                          job.get_id(),
-                          job.get_state(),
-                          job.get_project().get_name(),
-                          job.get_target(),
-                          suffix,
-                          error);
-
-        self.log(&msg);
-    }
-
-    pub fn log_worker_job(&mut self, job: &Job) {
-        let start = if job.has_build_started_at() {
-            job.get_build_started_at()
-               .parse::<DateTime<Utc>>()
-               .unwrap_or_else(|_| Utc::now())
-               .format("%Y-%m-%d %H:%M:%S")
-               .to_string()
-        } else {
-            "".to_string()
-        };
-
-        let stop = if job.has_build_finished_at() {
-            job.get_build_finished_at()
-               .parse::<DateTime<Utc>>()
-               .unwrap_or_else(|_| Utc::now())
-               .format("%Y-%m-%d %H:%M:%S")
-               .to_string()
-        } else {
-            "".to_string()
-        };
-
-        let msg = format!("W,{},{},{:?},{},{},,,{},{},{:?}",
-                          job.get_owner_id(),
-                          job.get_id(),
-                          job.get_state(),
-                          job.get_project().get_name(),
-                          job.get_target(),
-                          start,
-                          stop,
-                          job.get_error());
-        self.log(&msg);
     }
 }
 
