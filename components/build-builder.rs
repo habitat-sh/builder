a917a05efb32e537d769eb960ae307999e2f3e4e
sougata-progress <sougatab@progress.com>
2025-03-17 12:35:36 +0000

    Static linking issue WIP
    Signed-off-by: sougata-progress <sougatab@progress.com>

--- components/build-builder.rs
+++ components/build-builder.rs
@@ -1,4 +1,9 @@
 // Inline common build behavior
 include!("libbuild.rs");
 
-fn main() { builder::common(); }
+fn main() { 
+    builder::common(); 
+    println!("cargo:rustc-link-lib=static=crypto");
+    println!("cargo:rustc-link-lib=static=ssl");
+    println!("cargo:rustc-link-lib=dylib=dl");
+}
