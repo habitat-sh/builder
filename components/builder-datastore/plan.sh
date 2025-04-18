4b95a46c879c7e32caf8e9315af807e67ed85629
Jason Heath <jason.heath@progress.com>
2025-04-18 17:35:45 -0400

    wip - now compltes upgrade and subsequent install, lots 'o testing ahead
    Signed-off-by: Jason Heath <jason.heath@progress.com>

--- components/builder-datastore/plan.sh
+++ components/builder-datastore/plan.sh
@@ -32,34 +32,9 @@ do_before() {
 }
 
 do_build() {
-  # shellcheck disable=2154
-  # ld manpage: "If -rpath is not used when linking an ELF
-  # executable, the contents of the environment variable LD_RUN_PATH
-  # will be used if it is defined"
-  ./configure --disable-rpath \
-    --with-openssl \
-    --prefix="$pkg_prefix" \
-    --with-uuid=ossp \
-    --with-includes="$LD_INCLUDE_PATH" \
-    --with-libraries="$LD_LIBRARY_PATH" \
-    --sysconfdir="$pkg_svc_config_path" \
-    --localstatedir="$pkg_svc_var_path"
-  make world
-
-  # semver can't be built until after postgresql is installed to $pkg_prefix
+  return 0
 }
 
 do_install() {
-  make install-world
-
-  # make and install semver extension
-  export PATH="${PATH}:${pkg_prefix}/bin"
-  build_line "Added postgresql binaries to PATH: ${pkg_prefix}/bin"
-
-  pushd "$ext_semver_cache_path" >/dev/null || exit
-  build_line "Building ${ext_semver_dirname}"
-  make
-  build_line "Installing ${ext_semver_dirname}"
-  make install
-  popd >/dev/null || exit
+  return 0
 }
