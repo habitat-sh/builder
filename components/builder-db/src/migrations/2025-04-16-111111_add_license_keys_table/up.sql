5758be317ed9bfe65b6113113bcd2b7306ac7f65
sougata-progress <sougatab@progress.com>
2025-04-22 01:22:47 +0000

    Updated migratons
    Signed-off-by: sougata-progress <sougatab@progress.com>

--- components/builder-db/src/migrations/2025-04-16-111111_add_license_keys_table/up.sql
+++ components/builder-db/src/migrations/2025-04-16-111111_add_license_keys_table/up.sql
@@ -2,8 +2,10 @@ CREATE SEQUENCE IF NOT EXISTS license_keys_id_seq;
 
 CREATE TABLE IF NOT EXISTS license_keys (
     id bigint DEFAULT next_id_v1('license_keys_id_seq') PRIMARY KEY NOT NULL,
-    account_id bigint NOT NULL,
+    account_id bigint UNIQUE NOT NULL,
     license_key text UNIQUE NOT NULL,
     expiration_date text NOT NULL,
     created_at timestamp with time zone DEFAULT now()
 );
+
+ALTER TABLE license_keys ADD CONSTRAINT license_keys_account_id_key UNIQUE (account_id);
