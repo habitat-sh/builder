471275f58040b2d6e30d5257f6692f7312b2f973
sougata-progress <sougatab@progress.com>
2025-05-27 23:24:43 +0000

    Fixed struct names
    Signed-off-by: sougata-progress <sougatab@progress.com>

--- components/builder-db/src/schema/sql_types.rs
+++ components/builder-db/src/schema/sql_types.rs
@@ -4,24 +4,24 @@ use diesel::{query_builder::QueryId,
 /// Backing Postgres enum for PackageVisibility
 #[derive(SqlType, QueryId)]
 #[diesel(postgres_type(name = "origin_package_visibility"))]
-pub struct origin_package_visibility;
+pub struct OriginPackageVisibility;
 
 /// Backing Postgres enum for PackageChannelOperation
 #[derive(SqlType, QueryId)]
 #[diesel(postgres_type(name = "package_channel_operation"))]
-pub struct package_channel_operation;
+pub struct PackageChannelOperation;
 
 /// Backing Postgres enum for PackageChannelTrigger
 #[derive(SqlType, QueryId)]
 #[diesel(postgres_type(name = "package_channel_trigger"))]
-pub struct package_channel_trigger;
+pub struct PackageChannelTrigger;
 
 /// Backing Postgres enum for audit_origin.operation
 #[derive(SqlType, QueryId)]
 #[diesel(postgres_type(name = "origin_operation"))]
-pub struct origin_operation;
+pub struct OriginOperation;
 
 /// Backing Postgres enum for origin_members.member_role
 #[derive(SqlType, QueryId)]
 #[diesel(postgres_type(name = "origin_member_role"))]
-pub struct origin_member_role;
+pub struct OriginMemberRole;
