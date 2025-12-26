7a3caef7c0d3b2c6eb4bea5f7a5ec93610145978
Matt Wrock <matt@mattwrock.com>
2025-12-26 15:04:19 -0800

    put stuff in the right place
    Signed-off-by: Matt Wrock <matt@mattwrock.com>

--- components/builder-datastore/habitat-dev/plan.sh
+++ components/builder-datastore/habitat-dev/plan.sh
@@ -3,17 +3,14 @@
 source ../plan.sh
 pkg_exposes=()
 do_install() {
-  local pkg_path
-  pkg_path=$(hab pkg path habitat/"$pkg_name")
-
   build_line "Copying new config into package"
-  cp -v "../default.toml" "$pkg_path/default.toml"
-  cp -v "../config/pg_hba.conf" "$pkg_path/config/pg_hba.conf"
-  cp -v "../config/pg_ident.conf" "$pkg_path/config/pg_ident.conf"
-  cp -v "../config/postgresql.conf" "$pkg_path/config/postgresql.conf"
+  cp -v "../default.toml" "$pkg_prefix/default.toml"
+  cp -v "../config/pg_hba.conf" "$pkg_prefix/config/pg_hba.conf"
+  cp -v "../config/pg_ident.conf" "$pkg_prefix/config/pg_ident.conf"
+  cp -v "../config/postgresql.conf" "$pkg_prefix/config/postgresql.conf"
 
   build_line "Copying run hooks into package"
   for hook in ../hooks/*; do
-    cp -v "$hook" "$pkg_path/hooks/$(basename "$hook")"
+    cp -v "$hook" "$pkg_prefix/hooks/$(basename "$hook")"
   done
 }
