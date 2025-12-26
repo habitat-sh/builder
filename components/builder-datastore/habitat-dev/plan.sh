87f645ff825f76830e4a4007a040e5279a46e5a3
Matt Wrock <matt@mattwrock.com>
2025-12-26 14:16:56 -0800

    copying hooks and config for datastore
    Signed-off-by: Matt Wrock <matt@mattwrock.com>

--- components/builder-datastore/habitat-dev/plan.sh
+++ components/builder-datastore/habitat-dev/plan.sh
@@ -1,3 +1,18 @@
 #!/bin/bash
 
 source ../plan.sh
+do_install() {
+  local pkg_path
+  pkg_path=$(hab pkg path habitat/"$pkg_name")
+
+  build_line "Copying new config into package"
+  cp -v "../default.toml" "$pkg_path/default.toml"
+  cp -v "../config/pg_hba.toml" "$pkg_path/config/pg_hba.toml"
+  cp -v "../config/pg_ident.toml" "$pkg_path/config/pg_ident.toml"
+  cp -v "../config/postgresql.toml" "$pkg_path/config/postgresql.toml"
+
+  build_line "Copying run hooks into package"
+  for hook in ../hooks/*; do
+    cp -v "$hook" "$pkg_path/hooks/$(basename "$hook")"
+  done
+}
